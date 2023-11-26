192f443ef8162f26ecab11c98a7c6f708f88a6bf
Lucas <chanconan@gmail.com>
2023-11-26 15:46:42 +0800

    add sword module

---
+++ public/game_scripts/sword.js
@@ -0,0 +1,38 @@
+// This function defines the Sword module.
+const Sword = function (ctx, x, y) {
+
+    const sequences = {x: 112, y: 0, width: 16, height: 16, count: 1, timing: 200, loop: false};
+
+    // This is the sprite object of the gem created from the Sprite module.
+    const sprite = Sprite(ctx, x, y);
+
+    // The sprite object is configured for the fire sprite here.
+    sprite.setSequence(sequences)
+        .setScale(2)
+        .setShadowScale({x: 0.75, y: 0.2})
+        .useSheet("./images/object_sprites.png");
+
+    let birthTime = performance.now();
+
+    const getAge = function (now) {
+        return now - birthTime;
+    };
+
+    const randomize = function (area) {
+        /* Randomize the position */
+        const {x, y} = area.randomPoint();
+        sprite.setXY(x, y);
+    };
+
+    // The methods are returned as an object here.
+    return {
+        getXY: sprite.getXY,
+        setXY: sprite.setXY,
+        getBoundingBox: sprite.getBoundingBox,
+        draw: sprite.draw,
+        update: sprite.update,
+        remove: sprite.remove,
+        getAge: getAge,
+        randomize: randomize
+    };
+};
